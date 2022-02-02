456a3bba7db1526f5c234bd480b66a7a40109050
Loaphs <loaphs@gmail.com>
2022-02-02 09:51:22 -0700

    reset webscaper

--- WebScraper/webscrape.py
+++ WebScraper/webscrape.py
@@ -1,34 +1,9 @@
 import requests
-from tkinter import *
-from tkinter.ttk import *
+from bs4 import BeautifulSoup as bs
 
-class ScrollableFrame(Frame):
-    def __init__(self, container, *args, **kwargs):
-        super().__init__(container, *args, **kwargs)
-        canvas = Canvas(self)
-        scrollbar = Scrollbar(self, orient = 'vertical', command = canvas.yview)
-        self.scrollable_frame = Frame(canvas)
 
-        self.scrollable_frame.bind('<Configure>', lambda e: canvas.configure(scrollregion = canvas.bbox('all')))
+url='https://oxylabs.io/blog'
+response = requests.get(url)
 
-        canvas.create_window((0, 0), window = self.scrollable_frame, anchor = 'nw')
-
-        canvas.configure(yscrollcommand = scrollbar.set)
-
-        canvas.pack(side = 'left', fill = 'both', expand = True)
-        scrollbar.pack(side = 'right', fill = 'y')
-
-root = Tk()
-root.title('Web Scraper')
-root.geometry('600x300')
-
-frame = ScrollableFrame(root)
-
-url = 'https://realpython.github.io/fake-jobs/'
-page = requests.get(url)
-
-htmlRead = Label(frame.scrollable_frame, font = 'calibri 9', text = page.text)
-htmlRead.pack()
-
-frame.pack()
-root.mainloop()
+soup = bs(response.text, 'html.parser')
+print(soup.h1)
