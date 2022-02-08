c3a81deaa043d38b3631d0dafa4ca58f361258e9
Loaphs <loaphs@gmail.com>
2022-02-07 21:29:41 -0700

    updated webscrape.py

--- WebScraper/webscrape.py
+++ WebScraper/webscrape.py
@@ -2,3 +2,7 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver as wd
 
+driver = wd.Chrome(executable_path = 'D:\Loaphs\Downloads\chromedriver_win32\chromedriver.exe')
+driver.get('https://https://realpython.github.io/fake-jobs/')
+
+results = []
