2c8907baea49fb39fc08efb9b4aafaef918be846
Loaphs <loaphs@gmail.com>
2022-02-15 11:59:38 -0700

    added sprites to plataformer folder, created base for game

--- PyGame Platformer/jumpGame.py
+++ PyGame Platformer/jumpGame.py
@@ -1,33 +1,58 @@
-import pygame as py
-from pygame.locals import *
+import pygame
 
-py.init()
-vect = py.math.Vector2
-
-HEI = 450
 WID = 400
-ACC = 0.5
-FRIC = -0.12
-FPS = 60
+HEI = 300
+BG = (0, 0, 0)
 
-FramePerSec = py.time.Clock()
 
-displaysurface = py.display.set_mode((WID, HEI))
-py.display.set_caption("GAME")
 
-class Player(py.sprite.Sprite):
-    def __init_(self):
+#SPRITE
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, image, startx, starty):
         super().__init__()
-        self.surf = py.Surface((30, 30))
-        self.surf.fill((128, 255, 40))
-        self.rect = self.surf.get_rect(center = (10, 240))
 
-class Platform(py.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.surf = py.Surface((WID, 20))
-        self.surf.fill((255, 0, 0))
-        self.rect = self.surf.get_rect(center = (WID/2, HEI - 10))
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
 
-pt1 = Platform()
-p1 = Player()
+        self.rect.center = [startx, starty]
+
+    def update(self):
+            pass
+
+    def draw(self, screen):
+            screen.blit(self.image, self.rect)
+
+class Player(Sprite):
+    def __init__(self, startx, starty):
+        super().__init__("Python\\PyGame Platformer\\p1_front.png", startx, starty)
+
+class Box(Sprite):
+    def __init__(self, startx, starty):
+        super().__init__("Python\\PyGame Platformer\\stone.png", startx, starty)
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WID, HEI))
+    clock = pygame.time.Clock()
+
+    player = Player(100, 200)
+    
+    boxes = pygame.sprite.Group()
+    for bx in range(0, 400, 16):
+        boxes.add(Box(bx, 300))
+
+    #MAIN LOOP
+    while True:
+        pygame.event.pump()
+        player.update()
+
+        screen.fill(BG)
+        player.draw(screen)
+        boxes.draw(screen)
+        pygame.display.flip()
+
+        clock.tick(60)
+
+#INIT LOOP
+if __name__ == '__main__':
+    main()
